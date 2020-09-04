2a89da332ce65f4db0e95422ce41e1b89baaee4e
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2020-09-04 17:58:07 +0200

    Added all initial components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  actualRoute: string = "/home";
+  actualLanguage: string = "English"
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('en');
+  }
+
+  ngOnInit() {
+  }
+
+   /**
+   * Swittches the language.
+   * @param language
+   */
+  switchLanguage(language: string) {
+    this.translateService.use(language);
+    this.selectTheLanguageToPrint(language);
+  }
+
+  /**
+   * Selects the language to print.
+   * @param language
+   */
+  selectTheLanguageToPrint(language: string) {
+    switch (language) {
+      case "en": {
+        this.actualLanguage = "English";
+        break;
+      }
+      case "fr": {
+        this.actualLanguage = "Français";
+        break;
+      }
+    }
+  }
+
+  /**
+   * Sets the actual root.
+   * @param actualRoute
+   */
+  setActualRoute(actualRoute: string) {
+    this.actualRoute = actualRoute;
+  }
+}
